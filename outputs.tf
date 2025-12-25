e3be2951d1c3ade1fed1cb108e74c90736a43a48
22SkiTTleS22 <iliy220599@mail.ru>
2025-12-25 15:46:42 +0400

    task4

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+# Объявите в файле outputs.tf один output , содержащий: instance_name, 
+# external_ip, fqdn для каждой из ВМ в удобном лично для вас формате.
+
+output "output" {
+  value = {
+    name1 = yandex_compute_instance.vm_db_name
+    ip1 = yandex_compute_instance.vm_db_name.network_interface.[0].nat_ip_address
+    fqdn = yandex_compute_instance.platform
+    name2 = yandex_compute_instance.vm_web_name
+    ip2 = yandex_compute_instance.vm_web_name.network_interface.[0].nat_ip_address
+  }
+}
