15bd008001684395f3cc9d27ee99e0a9cafe40c8
22SkiTTleS22 <iliy220599@mail.ru>
2025-12-25 15:50:23 +0400

    task4.2

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,11 @@
 
 output "output" {
   value = {
-    name1 = yandex_compute_instance.vm_db_name
-    ip1 = yandex_compute_instance.vm_db_name.network_interface[0].nat_ip_address
-    fqdn = yandex_compute_instance.platform
-    name2 = yandex_compute_instance.vm_web_name
-    ip2 = yandex_compute_instance.vm_web_name.network_interface[0].nat_ip_address
+    web_name = yandex_compute_instance.platform.name
+    web_ip = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+    web_fqdn = yandex_compute_instance.platform.fqdn
+    db_name = yandex_compute_instance.platform2.name
+    db_ip = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+    db_fqdn = yandex_compute_instance.platform2.fqdn
   }
 }
